d85d87d379ce57ce63eea4e5f7513a10ef9383e8
Kajal Ranjan <mskajalranjan@gmail.com>
2025-03-29 23:11:01 -0700

    updated moodTrackerSection
    -

--- wellnesshub/src/app/components/MoodTrackerSection.tsx
+++ wellnesshub/src/app/components/MoodTrackerSection.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 
 const MoodTrackerSection = () => {
-    const [mood, setMood] = useState<number>(10); //this the default mood
+    const [mood, setMood] = useState<number>(5); //this the default mood
 
 
 useEffect(() => {
@@ -28,20 +28,21 @@ const handleMoodChange = async (newMood: number) => {
 
   return (
     <div>
-        <h2> mood tracker </h2>
+        <h2>Mood Tracker</h2>
         <input 
             type="number"
-            min="1"
+            min="0"
             max="10"
             value={mood} 
             onChange={(e) => handleMoodChange(Number(e.target.value))}
+            className="w-20 h-8  text-s p-2 border border-[#3b2f2a] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#3b2f2a] mt-4 mb-4"
       />
 
         <div
         style={{
             width: "100%",
             height: "20px",
-            background: "#b0c4b1",
+            background: "#3b2f2a", 
             overflow: "hidden",
         }}
     >
@@ -49,7 +50,7 @@ const handleMoodChange = async (newMood: number) => {
         style={{
             width: `${(mood/10)*100}%`,
             height: "100%",
-            background: "#4a5759",
+            background: "#B0C4B1", 
         }}
         />
         </div>
