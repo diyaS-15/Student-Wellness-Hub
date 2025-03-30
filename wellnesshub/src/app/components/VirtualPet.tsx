b5517bdf378a73471a033fe3e19625ed766dff2e
diyaS-15 <122123763+diyaS-15@users.noreply.github.com>
2025-03-29 23:36:40 -0700

    bubble image added

--- wellnesshub/src/app/components/VirtualPet.tsx
+++ wellnesshub/src/app/components/VirtualPet.tsx
@@ -2,6 +2,12 @@ import Image from "next/image";
 export default function VirtualPet() {
     return (
         <div>
+            <Image className="float-center"
+            src="/images/speechbubble.png"
+            alt="speech bubble background"
+            width={175}
+            height={200}
+            />
             <Image className="float-right"
             src="/images/cat.png"
             alt="Bo the virtual cat pet"
